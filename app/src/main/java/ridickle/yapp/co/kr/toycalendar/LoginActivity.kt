2ca84669508bbaeadc775f673baea97cfef80edb
ridickle7 <ridickle7@gmail.com>
2018-12-23 14:51:03 +0900

    ToyCalendar
    1. first Updated

---
+++ app/src/main/java/ridickle/yapp/co/kr/toycalendar/LoginActivity.kt
@@ -0,0 +1,12 @@
+package ridickle.yapp.co.kr.toycalendar
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
