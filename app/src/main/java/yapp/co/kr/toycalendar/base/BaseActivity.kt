c7a939cbbbc6918d069d196441f25283548603ef
ridickle7 <ridickle7@gmail.com>
2019-03-14 20:12:11 +0900

    ToyCalendar
    1. Base Architecture 정의
    - Activity (* dialog 초기화 코드 빠져서 추가)
    - Fragment
    - Components
      - ViewPager
      - RecyclerView
      - SingleLiveEvent
      - ViewModel
      - Dialog
    
    2. 기본 resource 정의
    - color
    - dimens
    - style (NoActionBar)
    - drawable (Dialog)
    
    3. 토의 필요
      TODO 토의 내용1. onAttach / onDetach 작업을 Base에서 진행해주는 것이 맞는가?
      TODO 토의 내용2. 클릭 중복 방지를 이렇게 처리해주는 게 옳은 걸까?

--- app/src/main/java/yapp/co/kr/toycalendar/base/BaseActivity.kt
+++ app/src/main/java/yapp/co/kr/toycalendar/base/BaseActivity.kt
@@ -14,6 +14,8 @@ abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        dialog = BaseDialog(this)
+
         if (layoutRes != 0) {
             if (!isUseDatabinding)
                 setContentView(layoutRes)
