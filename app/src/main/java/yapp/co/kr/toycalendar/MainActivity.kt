85a7398c88511f94d3db0f2efa21cf45f0c8f2df
sk392 <fsd.latte@gmail.com>
2019-03-23 15:16:22 +0900

    메인 액티비티 추가

---
+++ app/src/main/java/yapp/co/kr/toycalendar/MainActivity.kt
@@ -0,0 +1,13 @@
+package yapp.co.kr.toycalendar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import yapp.toycalendar.co.kr.toycalendar.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
