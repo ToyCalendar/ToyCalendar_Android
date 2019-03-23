53316d8f856e7ddf0b98243df1449e0b811060d9
sk392 <fsd.latte@gmail.com>
2019-03-23 14:32:41 +0900

    켈린더 데이터소스 및 더미 데이터 추가

---
+++ app/src/main/java/yapp/co/kr/toycalendar/calendar/data/datasource/ScheduleMockDatasource.kt
@@ -0,0 +1,49 @@
+package yapp.co.kr.toycalendar.calendar.data.datasource
+
+import com.google.gson.Gson
+import io.reactivex.Single
+import yapp.co.kr.toycalendar.App
+import yapp.co.kr.toycalendar.calendar.entity.Schedule
+import yapp.co.kr.toycalendar.calendar.entity.ScheduleResult
+import java.io.InputStreamReader
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ScheduleMockDatasource : ScheduleDataSource {
+
+    private val scheduleResultFileName = "test_schedule_dummy.json"
+    override fun getSchedule(monthList: List<String>): Single<ScheduleResult> {
+        return Single.create<ScheduleResult> {
+            it.onSuccess(getFilteredInfo(monthList,Gson().fromJson(
+                    InputStreamReader(App.getApp()!!.assets.open(scheduleResultFileName)),
+                    ScheduleResult::class.java)))
+        }.subscribeOn(io.reactivex.schedulers.Schedulers.io())
+    }
+
+
+    private fun getFilteredInfo(monthList: List<String>, scheduleResult: ScheduleResult): ScheduleResult {
+        val list = mutableListOf<Schedule>()
+        val monthDateFormat = SimpleDateFormat("yyyy-MM")
+        val scheduleDateFormat = SimpleDateFormat("yyyy-MM-dd")
+
+        scheduleResult.schedules.forEach { schedule ->
+            for (s in monthList) {
+                val beforeDate = monthDateFormat.parse(s)
+                val afterDate = Calendar.getInstance().apply {
+                    time = beforeDate
+                    add(Calendar.MONTH, 1)
+                }.time
+
+                val startDate = scheduleDateFormat.parse(schedule.startDate)
+                val endDate = scheduleDateFormat.parse(schedule.endDate)
+
+                if ((endDate.after(beforeDate) && endDate.before(afterDate)) || (startDate.after(beforeDate) && startDate.before(afterDate))) {
+                    list.add(schedule)
+                    break
+                }
+            }
+        }
+        return ScheduleResult(list)
+    }
+
+}
