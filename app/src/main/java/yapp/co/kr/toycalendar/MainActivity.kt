6ee783216baf5db657b66c92b93274583e372a90
sk392 <fsd.latte@gmail.com>
2019-05-19 12:02:31 +0900

    MainActivity에 MonthView추가

--- app/src/main/java/yapp/co/kr/toycalendar/MainActivity.kt
+++ app/src/main/java/yapp/co/kr/toycalendar/MainActivity.kt
@@ -1,13 +1,17 @@
 package yapp.co.kr.toycalendar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import yapp.co.kr.toycalendar.calendar.MonthType
+import yapp.co.kr.toycalendar.calendar.MonthViewModel
+import yapp.co.kr.toycalendar.calendar.widget.MonthView
 import yapp.toycalendar.co.kr.toycalendar.R
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val monthView = findViewById<MonthView>(R.id.month_view)
+        monthView.setViewModel(MonthViewModel(2019, 5, MonthType.MyCalendar))
     }
 }
