2ce32901507db70e1cbe946a13b5fb92108c60a9
sk392 <fsd.latte@gmail.com>
2019-05-19 12:01:50 +0900

    더미로부터 스케쥴 가져오는 UseCase 및 Datasource 수정

--- app/src/main/java/yapp/co/kr/toycalendar/calendar/domain/usecase/GetSchedules.kt
+++ app/src/main/java/yapp/co/kr/toycalendar/calendar/domain/usecase/GetSchedules.kt
@@ -1,19 +1,25 @@
 package yapp.co.kr.toycalendar.calendar.domain.usecase
 
 import io.reactivex.Scheduler
+import yapp.co.kr.toycalendar.calendar.MonthType
 import yapp.co.kr.toycalendar.calendar.domain.repository.CalendarRepository
 import yapp.co.kr.toycalendar.calendar.entity.Schedule
 
 class GetSchedules(val calendarRepository: CalendarRepository, val scheduler: Scheduler) : RxUseCase() {
-    operator fun invoke(monthList : List<String>, success: (schedules: List<Schedule>) -> Unit, error: (throwable: Throwable) -> Unit, force: Boolean = false) {
-        disposable = calendarRepository.getSchedules(force,monthList, scheduler, {
-            success(it.schedules)
+    operator fun invoke(monthList: List<String>, type: MonthType, success: (schedules: List<Schedule?>?) -> Unit, error: (throwable: Throwable) -> Unit, force: Boolean = false) {
+        disposable = calendarRepository.getSchedules(force, monthList, scheduler, {
+            when (type) {
+                MonthType.PartnerCalendar -> success(it.data?.partnerSchedules)
+                MonthType.MyCalendar -> success(it.data?.mySchedules)
+            }
         }, error)
     }
 
-    operator fun invoke(monthList : String, success: (schedules: List<Schedule>) -> Unit, error: (throwable: Throwable) -> Unit, force: Boolean = false) {
-        disposable = calendarRepository.getSchedules(force, listOf(monthList), scheduler, {
-            success(it.schedules)
-        }, error)
+    operator fun invoke(month: String, type: MonthType, success: (schedules: List<Schedule?>?) -> Unit, error: (throwable: Throwable) -> Unit, force: Boolean = false) {
+        disposable = calendarRepository.getSchedules(force, listOf(month), scheduler, {
+            when (type) {
+                MonthType.PartnerCalendar -> success(it.data?.partnerSchedules)
+                MonthType.MyCalendar -> success(it.data?.mySchedules)
+            }        }, error)
     }
 }
