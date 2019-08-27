7688487d367a94a8a4824300294e9276600ce957
sk392 <fsd.latte@gmail.com>
2019-08-27 21:50:17 +0900

    유즈케이스에서 disposable을 리턴하도록 변경

--- app/src/main/java/yapp/co/kr/toycalendar/calendar/domain/usecase/GetSchedules.kt
+++ app/src/main/java/yapp/co/kr/toycalendar/calendar/domain/usecase/GetSchedules.kt
@@ -1,13 +1,14 @@
 package yapp.co.kr.toycalendar.calendar.domain.usecase
 
 import io.reactivex.Scheduler
+import io.reactivex.disposables.Disposable
 import yapp.co.kr.toycalendar.calendar.MonthType
 import yapp.co.kr.toycalendar.calendar.domain.repository.CalendarRepository
 import yapp.co.kr.toycalendar.calendar.entity.Schedule
 
-class GetSchedules(val calendarRepository: CalendarRepository, val scheduler: Scheduler) : RxUseCase() {
-    operator fun invoke(monthList: List<String>, type: MonthType, success: (schedules: List<Schedule?>?) -> Unit, error: (throwable: Throwable) -> Unit, force: Boolean = false) {
-        disposable = calendarRepository.getSchedules(force, monthList, scheduler, {
+class GetSchedules(private val calendarRepository: CalendarRepository, private val scheduler: Scheduler) : RxUseCase() {
+    operator fun invoke(monthList: List<String>, type: MonthType, success: (schedules: List<Schedule?>?) -> Unit, error: (throwable: Throwable) -> Unit, force: Boolean = false) : Disposable {
+       return calendarRepository.getSchedules(force, monthList, scheduler, {
             when (type) {
                 MonthType.PartnerCalendar -> success(it.data?.partnerSchedules)
                 MonthType.MyCalendar -> success(it.data?.mySchedules)
@@ -15,8 +16,8 @@ class GetSchedules(val calendarRepository: CalendarRepository, val scheduler: Sc
         }, error)
     }
 
-    operator fun invoke(month: String, type: MonthType, success: (schedules: List<Schedule?>?) -> Unit, error: (throwable: Throwable) -> Unit, force: Boolean = false) {
-        disposable = calendarRepository.getSchedules(force, listOf(month), scheduler, {
+    operator fun invoke(month: String, type: MonthType, success: (schedules: List<Schedule?>?) -> Unit, error: (throwable: Throwable) -> Unit, force: Boolean = false) : Disposable {
+        return calendarRepository.getSchedules(force, listOf(month), scheduler, {
             when (type) {
                 MonthType.PartnerCalendar -> success(it.data?.partnerSchedules)
                 MonthType.MyCalendar -> success(it.data?.mySchedules)
